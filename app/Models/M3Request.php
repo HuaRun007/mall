cdb09521ddd1a6d6f217b1ff175fedf5af21c95a
huarun <huarun@360container.com>
2019-09-12 14:15:51 +0800

    更新返回状态的方法

--- app/Models/M3Request.php
+++ app/Models/M3Request.php
@@ -32,4 +32,50 @@ class M3Request
         echo json_encode($response);
         exit;
     }
+
+    public static function responseError($message = null){
+        header('Content-Type: application/json; charset=utf-8');
+        if (is_array($message)){
+            $error = '';
+            foreach ($message as $value){
+                foreach ($value as $k => $v){
+                    $error .=$v .',';
+                }
+            }
+            $message = $error;
+        }
+
+        if(empty($message)){
+            $message="操作失败";
+        }
+        $response = array(
+            "code"	=>	300,
+            "title"=> "操作提示",
+            "message"=> $message
+        );
+        echo json_encode($response);
+        exit;
+    }
+
+    public static function responseObj($data = null)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        echo  json_encode($data,JSON_UNESCAPED_UNICODE);
+        exit;
+
+    }
+
+    public static function responseCode($statusCode,$message = null){
+        header('Content-Type: application/json; charset=utf-8');
+        if(empty($message)){
+            $message="恭喜你，操作成功";
+        }
+        $response = array(
+            "code"	=>	$statusCode,
+            "title"=> "操作提示",
+            "message"=> $message
+        );
+        echo json_encode($response);
+        exit;
+    }
 }
